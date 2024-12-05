ffa35089021684cec224c49685b5f09be7717cd9
Alexander Linus Grübling <6138@htl.rennweg.at>
2024-12-05 16:08:50 +0100

    [TASK] Add ruleX interactions, add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# PolyVis
+Repository of PolyVis, a project created as part of my bachelor thesis in Bioinformatics. It aims at visualizing data from Polysomnography and helping to diagnose the different forms of sleep apnea.
+
+## Screenshots
+![img.png](Documentation/img.png)
--- app.js
+++ app.js
@@ -9,10 +9,10 @@ import { loadData } from './dataUtils.js';
 
 console.log(signalsUrl)
 vg.coordinator().databaseConnector(vg.wasmConnector());
-vg.coordinator().exec(vg.loadCSV("signal", "http://127.0.0.1:5173" + signalsUrl));
-vg.coordinator().exec(vg.loadCSV("hypn", "http://127.0.0.1:5173" + hypnUrl));
-vg.coordinator().exec(vg.loadCSV("arou", "http://127.0.0.1:5173" + arouUrl));
-vg.coordinator().exec(vg.loadCSV("resp", "http://127.0.0.1:5173" + respUrl));
+vg.coordinator().exec(vg.loadCSV("signal", "http://localhost:5173" + signalsUrl));
+vg.coordinator().exec(vg.loadCSV("hypn", "http://localhost:5173" + hypnUrl));
+vg.coordinator().exec(vg.loadCSV("arou", "http://localhost:5173" + arouUrl));
+vg.coordinator().exec(vg.loadCSV("resp", "http://localhost:5173" + respUrl));
 
 const $xs = vg.Selection.intersect();
 const $dispArou = vg.Param.value(0);
@@ -27,66 +27,77 @@ document.getElementById('container2').appendChild(
             vg.from("hypn"),
             {x: "Sample#", y: "Aux"}
         ),
-        // vg.nearestX({as: $hypnPoint}),
-        // vg.ruleX({x: $hypnPoint}),
-        // // vg.textX({x: $point, text: $point, frameAnchor: "top", lineAnchor: "bottom"}),
-        // vg.height(400)
+        vg.nearestX({as: $hypnPoint}),
+        vg.ruleX({x: $hypnPoint}),
+        // vg.textX({x: $hypnPoint, text: $hypnPoint, frameAnchor: "top", lineAnchor: "bottom"}),
+        vg.textX(
+            vg.from("hypn"),
+            {
+                x: $hypnPoint,
+                text: "Aux",
+                frameAnchor: "top",
+                // lineAnchor: "bottom",
+                dy: -8,
+                select: "nearestX"
+            }
+        ),
+        vg.height(400)
     )
 );
 
 document.getElementById('saO2').appendChild(
-        vg.plot(
-            vg.line(
-                vg.from("signal", {filterBy: $oxygenThreshold}),
-                {
-                    x: "time",
-                    y: "SaO2",
-                }
-            ),
-            // vg.ruleX({x: $point}),
-            // vg.textX({x: $point, text: $point, frameAnchor: "top", lineAnchor: "bottom", dy: -7}),
-            // vg.ruleX(
-            //     vg.from("arou"),
-            //     { x: "Sample#", stroke: "#cba6f7", strokeOpacity: $dispArou }
-            // ),
-            // vg.ruleX(
-            //     vg.from("resp"),
-            //     { x: "Sample#", stroke: "#a6e3a1", strokeOpacity: $dispResp }
-            // ),
-            // vg.panZoomX({x: $xs}),
-            // vg.nearestX({as: $point})
-        )
+    vg.plot(
+        vg.line(
+            vg.from("signal"),
+            {
+                x: "time",
+                y: "SaO2",
+            }
+        ),
+        vg.nearestX({as: $point}),
+        vg.ruleX({x: $point}),
+        // vg.textX({x: $point, text: $point, frameAnchor: "top", lineAnchor: "bottom", dy: -7}),
+        // vg.ruleX(
+        //     vg.from("arou"),
+        //     { x: "Sample#", stroke: "#cba6f7", strokeOpacity: $dispArou }
+        // ),
+        // vg.ruleX(
+        //     vg.from("resp"),
+        //     { x: "Sample#", stroke: "#a6e3a1", strokeOpacity: $dispResp }
+        // ),
+        vg.panZoomX({x: $xs}),
+    )
 );
 
 // document.getElementById('saO2').appendChild(vg.slider({ select: "interval", from: "signal", column: "SaO2", as:  $oxygenThreshold}));
 
-// document.getElementById('container').appendChild(
-//     vg.plot(
-//         vg.line(
-//             vg.from("signal"),
-//             { x: "time", y: "EEG" }
-//         ),
-//         vg.ruleX(
-//             vg.from("arou"),
-//             { x: "Sample#", stroke: "#cba6f7", strokeOpacity: $dispArou }
-//         ),
-//         vg.ruleX(
-//             vg.from("resp"),
-//             { x: "Sample#", stroke: "#a6e3a1", strokeOpacity: $dispResp }
-//         ),
-//         vg.panZoomX({x: $xs})
-//     )
-// )
+ document.getElementById('container').appendChild(
+     vg.plot(
+         vg.line(
+             vg.from("signal"),
+             { x: "time", y: "EEG" }
+         ),
+         vg.ruleX(
+             vg.from("arou"),
+             { x: "Sample#", stroke: "#cba6f7", strokeOpacity: $dispArou }
+         ),
+         vg.ruleX(
+             vg.from("resp"),
+             { x: "Sample#", stroke: "#a6e3a1", strokeOpacity: $dispResp }
+         ),
+         vg.panZoomX({x: $xs})
+     )
+)
 
-// document.getElementById('controls').appendChild(
-//     vg.menu({
-//         label: "Arousal Events",
-//         options: [{value: 0, label: "Hide"}, {value: 1, label: "Show"}], as: $dispArou}
-//     )
-// )
-// document.getElementById('controls').appendChild(
-//     vg.menu({
-//         label: "Respiratory Events",
-//         options: [{value: 0, label: "Hide"}, {value: 1, label: "Show"}], as: $dispResp}
-//     )
-// )
+document.getElementById('controls').appendChild(
+     vg.menu({
+         label: "Arousal Events",
+         options: [{value: 0, label: "Hide"}, {value: 1, label: "Show"}], as: $dispArou}
+     )
+)
+document.getElementById('controls').appendChild(
+     vg.menu({
+         label: "Respiratory Events",
+         options: [{value: 0, label: "Hide"}, {value: 1, label: "Show"}], as: $dispResp}
+     )
+)
